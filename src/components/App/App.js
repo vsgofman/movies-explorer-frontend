f0f4a87af537552917549b26e20034653cf68f11
Viktor Gofman <vsgof@yandex.ru>
2023-05-29 11:51:13 +0300

    feat: подключить шрифты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,7 +10,7 @@ import Register from '../Register/Register';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <div className='page'>
         <Routes>
           <Route path='/' element={
