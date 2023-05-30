0adf4aae51df131dfc39a223f77bbb5eb87e4d5b
Viktor Gofman <vsgof@yandex.ru>
2023-05-30 22:26:51 +0300

    feat: компонент SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,28 @@
 import './SearchForm.css';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
-import logo from '../../images/logo.svg';
+import iconInput from '../../images/icon__input-search.png';
+import iconButton from '../../images/icon__button-search.png';
 
 function SearchForm() {
   return (
-    <form className='search'>
-      <FilterCheckbox />
-    </form>
+    <section className='search'>
+      <form className='search__form'>
+        <img className='search__img' src={iconInput} alt='иконка поиска' />
+        <input 
+        className='search__input'
+        id='search-input'
+        type='text'
+        name='search'
+        placeholder='Фильм'
+        />
+        <button className='search__button'>
+          <img className='button__img' src={iconButton} alt='иконка поиска' />
+        </button>
+        <FilterCheckbox />
+        <p className='search__text'>Короткометражки</p>
+      </form>
+    </section>
+
   )
 }
 
