6407a3cd20b30bb9d622058fe23b4da031986f35
Viktor Gofman <vsgof@yandex.ru>
2023-05-30 11:17:01 +0300

    feat: компонент Movies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,8 +1,12 @@
 import './Movies.css';
+import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
 
 function Movies() {
   return (
     <section className='movies'>
+      <SearchForm />
+      <MoviesCardList />
     </section>
   )
 }
