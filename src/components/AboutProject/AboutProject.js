5190612e01c68283f1d10bd9fb847d75034d2965
Viktor Gofman <vsgof@yandex.ru>
2023-06-01 16:16:35 +0300

    feat: адаптив для AboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -7,12 +7,12 @@ function AboutProject() {
       <RunningTitle text='О проекте' />
       <ul className='project__block'>
         <li className='project__item'>
-          <h3 className='item__title'>Дипломный проект включал 5 этапов</h3>
-          <p className='item__text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+          <h3 className='item__title'>Дипломный проект включал 5&nbsp;этапов</h3>
+          <p className='item__text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и&nbsp;финальные доработки.</p>
         </li>
         <li className='project__item'>
-          <h3 className='item__title'>На выполнение диплома ушло 5 недель</h3>
-          <p className='item__text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+          <h3 className='item__title'>На выполнение диплома ушло 5&nbsp;недель</h3>
+          <p className='item__text'>У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
         </li>
       </ul>
       <div className='project__time'>
