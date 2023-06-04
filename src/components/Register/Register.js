c3ca0b759b216b97cd2fe7d082996563292c3a17
Viktor Gofman <vsgof@yandex.ru>
2023-06-04 18:22:13 +0300

    feat: ошибки для StartPage

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -9,14 +9,14 @@ function Register() {
       textButtonRedirect='Войти'
       questionToRedirect='Уже зарегистрированы?'
       redirectTo='/signin'>
-      <>
+      <div className='start-page__input-block'>
         <p className='form__caption'>Имя</p>
         <input
           id="name-input"
           className="start-page__input"
           name="register-name" required
         />
-      </>
+      </div>
     </StartPage>
   )
 }
--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -9,22 +9,27 @@ function StartPage({ title, textButtonSubmit, textButtonRedirect, questionToRedi
       <h2 className="start-page__title">{title}</h2>
       <form className="form start-page__form" id="start-page__form">
         {children}
-        <p className='form__caption'>E-mail</p>
-        <input
-          id="email-input"
-          className="start-page__input"
-          type="email" name="email" required
-        />
-        <p className='form__caption'>Пароль</p>
-        <input
-          id="password-input"
-          className="start-page__input"
-          type="password" name="password" required
-        />
+        <div className='start-page__input-block'>
+          <p className='form__caption'>E-mail</p>
+          <input
+            id="email-input"
+            className="start-page__input"
+            type="email" name="email" required
+          />
+        </div>
+        <div className='start-page__input-block'>
+          <p className='form__caption'>Пароль</p>
+          <input
+            id="password-input"
+            className="start-page__input"
+            type="password" name="password" required
+          />
+          <span className='input__error'>Что-то пошло не так...</span>
+        </div>
       </form>
       <button
         className="start-page__button"
-        type="submit" 
+        type="submit"
         form="start-page__form"
         aria-label={textButtonSubmit}>{textButtonSubmit}
       </button>
