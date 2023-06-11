5c80bb433032f361043d7a51f0aaa33bfae1ee75
Viktor Gofman <vsgof@yandex.ru>
2023-06-11 16:30:27 +0300

    feat: отрисовка карточек в зависимости от ширины

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,12 +1,33 @@
+import { useState, useEffect } from 'react';
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
+
 function MoviesCardList({ movies }) {
+  const [amountCards, setAmountCards] = useState(12);
+  useEffect(() => {
+    let timer;
+    const changeAmountTimer = () => {
+      timer = setTimeout(changeAmountInWidth, 1000);
+    };
+    window.addEventListener("resize", changeAmountTimer);
+  });
+
+  function changeAmountInWidth() {
+    if (window.innerWidth < 651) {
+      return setAmountCards(5);
+    }
+    if (window.innerWidth < 918) {
+      return setAmountCards(8);
+    }
+    setAmountCards(12);
+  }
+
   return (
     <section className='movies-list'>
       <div className='movies-list__block'>
         {
-          movies.map((movie, i) => (
+          movies.slice(0, amountCards).map((movie, i) => (
             <MoviesCard movie={movie} key={movie._id} />
           ))
         }
