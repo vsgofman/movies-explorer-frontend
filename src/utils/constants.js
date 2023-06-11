a5dc27bf2d7af312234b5b08c88f7adefb049859
Viktor Gofman <vsgof@yandex.ru>
2023-06-11 15:17:32 +0300

    fix: состояние focus инпутов

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const URL_MOVIES = 'https://api.nomoreparties.co/beatfilm-movies';
-export const URL_MAIN = '';
+export const URL_MAIN = 'https://vsgof.movies-api.nomoredomains.monster';
 export const URL_LOCAL = '';
